f9ad43947ecf6303ea6a2b98265e28aa7ad31f62
mykhailo.kalinichenko <kalinichencomixail@gmail.com>
2024-05-27 16:13:03 +0200

    refactor lesson_1.py

--- lesson_1.py
+++ lesson_1.py
@@ -2,34 +2,45 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
+from selenium.webdriver.common.by import By
+
 
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
 
     
     def test_app_dynamics_job(self):
-        driver = self.driver
-        driver.get("https://medikom.ua/")
-        driver.find_element_by_xpath("//a[@href='/doctor/']")
-        driver.find_element_by_xpath("//img[@alt='logo']")
-    
+        wd = self.wd
+        self.open_home_padge(wd)
+        self.open_doctors_page(wd)
+        wd.find_element(By.XPATH, "//img[@alt='logo']").click()
+
+    def open_doctors_page(self, wd):
+        # open doctors page
+        wd.find_element(By.XPATH, "//a[@href='/doctor/']").click()
+
+    def open_home_padge(self, wd):
+        # open home page
+        wd.get("https://medikom.ua/")
+
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
+        try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
+        try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
     
 
     
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
 
 if __name__ == "__main__":
     unittest.main()
